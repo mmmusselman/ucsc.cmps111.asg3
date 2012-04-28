54f3f419dd1ac235f9dd9bcfa1b7dc75dff2adbb
Matthew Musselman <mmusselm@ucsc.edu>
2012-04-28 05:01:40 +0000

    Made it pretty.

--- performanceTest.c
+++ performanceTest.c
@@ -3,14 +3,17 @@
 int main() {
 	
 	int alloc_id[513];
-	int alloc_size = 1024;
-	unsigned int freeListFlags[4] = {0x4, 0x4|0x08, 0x4|0x10, 0x4|0x18};
+	int numAllocators = sizeof(alloc_id)/sizeof(int);
 	int i;
 	
-	int numAllocators = sizeof(alloc_id)/sizeof(int);
 	
-	printf("numAllocators=%d\n", numAllocators);
-	
+	printf("*******************************************************************************\n");
+	printf("*\n");
+	printf("*		FreeList Performance Test\n");
+	printf("*\n");
+	printf("*******************************************************************************\n\n\n");
+	int alloc_size = 1024;
+	unsigned int freeListFlags[4] = {0x4, 0x4|0x08, 0x4|0x10, 0x4|0x18};
 	for(i=0; i<numAllocators; i++) {
 		if(NULL == (alloc_id[i] = meminit(alloc_size, freeListFlags[i%4], NULL, NULL)) ) {
 			printf("Error allocating id %d\n", i);
